ad11a113826e73b5574fcef04243a52a9de11d93
Louis Hémadou <louis.hemadou@eleves.enpc.fr>
2019-12-05 19:05:40 +0100

    euh c pour être à jour

--- code/source/main.cpp
+++ code/source/main.cpp
@@ -13,7 +13,7 @@ double gaussian(double x, double sigma, double mu){
 int main(int argc, char *argv[])
 {	
 	InitRandom();
-	int N = strtol(argv[1], nullptr, 0);;
+	/*int N = strtol(argv[1], nullptr, 0);;
 	double values1[N];
 	double values2[N];
 	for(int i=0; i<N; i++){
@@ -24,7 +24,6 @@ int main(int argc, char *argv[])
 	simplex s2(values2, N, '2');
 	int n_iter = strtol(argv[2], nullptr, 0);;
 	cout << "n_iter=" << n_iter << endl;
-	//double eps = (double) strtol(argv[2], nullptr, 0);;
 	double eps = 1/((double)3*N);
 	cout << "eps=" << eps << endl;
       	pi gamma = W(s1, s2, eps, n_iter);
@@ -34,8 +33,9 @@ int main(int argc, char *argv[])
 	double lambda = 1.;
 	simplex barycenter = bar(s1, s2, lambda, eps, n_iter);
 	barycenter.plot();
+	*/
 	//TEST SIMPLEXE IMAGE
-	simplex lena("lena.png", '3');
-	lena.export_to_img();
+	simplex square("square.png", '1');
+	square.export_to_img();
 	return 0;
 }
