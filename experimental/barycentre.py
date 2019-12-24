12a394f001738f30e1c855ed38c5923423ddac6e
Louis Hémadou <louis.hemadou@eleves.enpc.fr>
2019-12-24 15:39:23 +0100

    barycenter2

--- experimental/barycentre.py
+++ experimental/barycentre.py
@@ -63,6 +63,53 @@ def barycenter(p1, p2, lamb, eps, n_iter):
     print(max(bar2))
     return q1, q2, bar1, bar2, p
 
+
+def barycenter2(p1, p2, lamb, eps, n_iter):
+    lamb1 = lamb
+    lamb2 = 1 - lamb
+    m = p1.img_size**2
+    K = np.zeros((m, m))
+    for i in range(m):
+        for j in range(m):
+            x_i = (i+1/2)/m
+            x_j = (j+1/2)/m
+            dis = (x_i-x_j)**2
+            K[i, j] = np.exp(-dis/eps)
+    a1 = a2 = np.ones(m)
+    for l in range(n_iter):
+        # computing p(l)
+        p = np.multiply(np.power(np.dot(K.T,a1), lamb1), np.power(np.dot(K.T, a2), lamb2))
+        # computing bs(l+1)
+        b1 = np.divide(p, np.dot(K.T, a1))
+        b2 = np.divide(p, np.dot(K.T, a2))
+        # computing as(l+1)
+        a1 = np.divide(p1.values, np.dot(K, b1))
+        a2 = np.divide(p2.values, np.dot(K, b2))
+    return (lamb1*p1.constant + lamb2*p2.constant)*p
+
+def barycenter2_curves(y1, y2, lamb, eps, n_iter):
+    lamb1 = lamb
+    lamb2 = 1 - lamb
+    m = len(y1)
+    K = np.zeros((m, m))
+    for i in range(m):
+        for j in range(m):
+            x_i = (i+1/2)/m
+            x_j = (j+1/2)/m
+            dis = (x_i-x_j)**2
+            K[i, j] = np.exp(-dis/eps)
+    a1 = a2 = np.ones(m)
+    for l in range(n_iter):
+        # computing p(l)
+        p = np.multiply(np.power(np.dot(K.T,a1), lamb1), np.power(np.dot(K.T, a2), lamb2))
+        # computing bs(l+1)
+        b1 = np.divide(p, np.dot(K.T, a1))
+        b2 = np.divide(p, np.dot(K.T, a2))
+        # computing as(l+1)
+        a1 = np.divide(y1, np.dot(K, b1))
+        a2 = np.divide(y2, np.dot(K, b2))
+    return p
+
 T0 = time.time()
 
 SQUARE = cv2.imread('./SQUARE_32.png', 0)
@@ -71,19 +118,22 @@ CIRCLE = cv2.imread('./CIRCLE_32.png', 0)
 p1 = SimplexFromImage(SQUARE)
 p2 = SimplexFromImage(CIRCLE)
 
-lamb = 1
-eps = 2/p1.lenght
+lamb = 0.8
+eps = 2/(p1.lenght)
 n_iter = 100
-q1, q2, bar1, bar2, p = barycenter(p1, p2, lamb, eps, n_iter)
 print("temps écoulé = " + str(time.time()-T0))
-q1 = ImageFromSimplex(q1, p1.img_size)
-q1.show()
-q2 = ImageFromSimplex(q2, p2.img_size)
-q2.show()
-bar1 = ImageFromSimplex(bar1, p1.img_size)
-bar1.show()
-bar2 = ImageFromSimplex(bar2, p1.img_size)
-bar2.show()
-print("aaa")
-bar3 = ImageFromSimplex(p, p1.img_size)
-bar3.show()
+
+
+X = np.linspace(0,1,100)
+Y1 = [np.exp(x) for x in X]
+Y1 = Y1/sum(Y1)
+Y2 = [np.cos(2*np.pi*x)+1 for x in X]
+Y2 = Y2/sum(Y2)
+
+
+plt.figure(1)
+plt.plot(X, Y1, 'b')
+plt.plot(X, Y2, 'r')
+bar = barycenter2_curves(Y1, Y2, lamb, eps, n_iter)
+plt.plot(X, bar, 'g')
+plt.show()
