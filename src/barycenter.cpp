a8aadee59e72addcc7267bc7b10ebffd4226bdd2
Louis Lesueur <louis.lesueur@gmx.fr>
2020-02-10 15:16:17 +0100

    optimized K

--- src/barycenter.cpp
+++ src/barycenter.cpp
@@ -8,16 +8,15 @@ using namespace std;
 
 matrix<float> gen_K(int m, float eps){
   matrix<float> K(m,m);
-  #pragma omp parallel for
   for(int i=0; i<m; i++){
-      for(int j=0; j<m; j++){
+      for(int j=0; j<=i; j++){
           float x_i = ((float)i)/m;
           float x_j = ((float)j)/m;
           float dis = pow(x_i - x_j, 2);
-          K(i,j) = -dis/eps;
+          K(i,j) = exp(-dis/eps);
       }
   }
-  return make_symmetric(exp(K));
+  return make_symmetric(K);
 }
 
 //====================================================================
